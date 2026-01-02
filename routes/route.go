f6221c2ada3b5d9679ed334145e080609b8649d7
Sarthak Srivastava <SarthakSrivastava@MacBook-Pro.local>
2026-01-02 19:01:43 +0530

    adding routes

--- routes/route.go
+++ routes/route.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/phantom5803/E-commerce-website/controllers"
+)
+func UserRoutes(c *gin.Engine) {
+	c.POST("/users/signup",controllers.Signup())
+	c.POST("/users/login",controllers.Login())
+	c.GET("/admin/products",controllers.ProductViewAdmin())
+	c.POST("/admin/addproduct",controllers.AddProduct())
+	c.GET("/users/search",controllers.SearchProduct())
+}
+
